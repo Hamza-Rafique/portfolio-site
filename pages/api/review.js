780f5ce3c1f3ad9de1759fe0cf09b80f96388a72
Hamza Rafique <hamzarafique964@gmail.com>
2024-10-19 20:21:48 +0300

    fiveerr review updated

--- pages/api/review.js
+++ pages/api/review.js
@@ -1,33 +1,104 @@
-const review = [
+const reviews = [
     {
         id: 0,
-        clientName: 'Mustafash',
-        clientLocation: 'Egypt',
+        clientName: 'craigbradley954',
+        clientLocation: 'United States',
         clientSource: 'Fiverr',
-        clientReview: 'It is always nice to work with him. The work is finished quickly and he does more than is asked of him. It is also clear that he has a lot of knowledge of the work he does. I look forward to our next collaboration.'
+        clientReview: 'Great work thank you!'
+    },
+    {
+        id: 1,
+        clientName: 'naveed_shahid',
+        clientLocation: 'Pakistan',
+        clientSource: 'Fiverr',
+        clientReview: 'Hamza was excellent at fixing React/Next.js bugs. They were knowledgeable, efficient, and communicated well. Highly recommend!'
     },
     {
         id: 2,
-        clientName: 'Melissabrown255',
-        clientLocation: 'United States',
+        clientName: 'naveed_shahid',
+        clientLocation: 'Pakistan',
         clientSource: 'Fiverr',
-        clientReview: 'He is very good with communication and experienced website developer, great experience to work with him. '
+        clientReview: 'Hamza\'s contributions to the development were outstanding, demonstrating exceptional attention to detail and a level of professionalism that went above and beyond expectations.'
     },
     {
         id: 3,
-        clientName: 'Jake Corry',
-        clientLocation: 'Germany',
+        clientName: 'craigbradley954',
+        clientLocation: 'United States',
         clientSource: 'Fiverr',
-        clientReview: "Osama is extremely knowledgeable and helpful. He knows in and out of web app development . His understanding of different technologies and frameworks in the space is also very impressive. I hope I get the chance to work with him again"
+        clientReview: 'Hamza helped us work through some difficult UI / UX issues and was a huge help! I would recommend him for sure!'
     },
     {
         id: 4,
-        clientName: 'Blaise Labriola',
+        clientName: 'darth_vader_198',
+        clientLocation: 'Singapore',
+        clientSource: 'Fiverr',
+        clientReview: 'Hamza truly impressed me with his professionalism and exceptional documentation, EXCEEDING all my expectations. Highly recommend! 👍'
+    },
+    {
+        id: 5,
+        clientName: 'darth_vader_198',
+        clientLocation: 'Singapore',
+        clientSource: 'Fiverr',
+        clientReview: 'Rafique\'s work in storybook development was impeccable, showcasing remarkable attention to details and professionalism. Beyond the top-notch delivery, working with Hamza was a pleasure.'
+    },
+    {
+        id: 6,
+        clientName: 'tapplix',
+        clientLocation: 'United States',
+        clientSource: 'Fiverr',
+        clientReview: 'Hamza was amazing to work with and his work was quick! I am very satisfied.'
+    },
+    {
+        id: 7,
+        clientName: 'roniy1985',
+        clientLocation: 'United States',
+        clientSource: 'Fiverr',
+        clientReview: 'Top quality service! Clean code and very quick responses. Pleasure working with him.'
+    },
+    {
+        id: 8,
+        clientName: 'roniy1985',
+        clientLocation: 'United States',
+        clientSource: 'Fiverr',
+        clientReview: 'Love the new designs of my app!!! Hamza and his team are very talented!!'
+    },
+    {
+        id: 9,
+        clientName: 'xotonymous',
         clientLocation: 'France',
         clientSource: 'Fiverr',
-        clientReview: 'I have worked with Osama and he is very professional and excellent software  developer. I admire his speed of development and quality of work when given an assignment.'
+        clientReview: 'Great to work with!'
     },
-]
+    {
+        id: 10,
+        clientName: 'haha0101',
+        clientLocation: 'United States',
+        clientSource: 'Fiverr',
+        clientReview: 'Wow, Hamza went above and beyond with my order. I highly recommend working with him for all your projects!'
+    },
+    {
+        id: 11,
+        clientName: 'kppsemp001',
+        clientLocation: 'United States',
+        clientSource: 'Fiverr',
+        clientReview: 'Good seller. Addressed all concerns and delivered exactly what I needed on time.'
+    },
+    {
+        id: 12,
+        clientName: 'abdulrahmannn99',
+        clientLocation: 'Qatar',
+        clientSource: 'Fiverr',
+        clientReview: 'A great work done, highly recommended.'
+    },
+    {
+        id: 13,
+        clientName: 'natede',
+        clientLocation: 'United States',
+        clientSource: 'Fiverr',
+        clientReview: 'Work was completed in a timely manner.'
+    }
+];
+
 export default function handler(req, res) {
     res.status(200).json(review)
 }
